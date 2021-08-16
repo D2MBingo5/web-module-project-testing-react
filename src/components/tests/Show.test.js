3c841775bef297652c5bbaa28ebd2cfc9b40fc5b
Leu Family <romanianfobjr@gmail.com>
2021-08-16 15:57:10 -0500

    Show.test.js - test 1 complete

--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -6,9 +6,16 @@ import Show from './../Show';
 
 const testShow = {
     //add in approprate test data structure here.
+    name: 'test name',
+    summary: 'test summary',
+    seasons: [{id:0, name: "Season 1", episodes: []}, 
+    {id:1, name: "Season 2", episodes: []}, 
+    {id:2, name: "Season 3", episodes: []}, 
+    {id:3, name: "Season 4", episodes: []}]
 }
 
 test('renders testShow and no selected Season without errors', ()=>{
+    render(<Show show={testShow} selectedSeason={'none'}/>)
 });
 
 test('renders Loading component when prop show is null', () => {
@@ -25,8 +32,13 @@ test('component renders when no seasons are selected and when rerenders with a s
 
 //Tasks:
 //1. Build an example data structure that contains the show data in the correct format. A show should contain a name, a summary and an array of seasons, each with a id, name and (empty) list of episodes within them. Use console.logs within the client code if you need to to verify the structure of show data.
+
 //2. Test that the Show component renders when your test data is passed in through show and "none" is passed in through selectedSeason.
+
 //3. Test that the Loading component displays when null is passed into the show prop (look at the Loading component to see how to test for it's existance)
+
 //4. Test that when your test data is passed through the show prop, the same number of season select options appears as there are seasons in your test data.
+
 //5. Test that when an item is selected, the handleSelect function is called. Look at your code to see how to get access to the select Dom element and userEvent reference materials to see how to trigger a selection.
+
 //6. Test that the episode component DOES NOT render when the selectedSeason props is "none" and DOES render the episode component when the selectedSeason prop has a valid season index.
